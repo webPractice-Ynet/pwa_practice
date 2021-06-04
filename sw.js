2aad9d2efb0697b4fc0ab4aa5c646386a6f5961e
k-nakabayashi <123windam@gmail.com>
2021-06-04 12:56:17 +0900

    main

--- sw.js
+++ sw.js
@@ -4,9 +4,9 @@
 // キャッシュ名とキャッシュファイルの指定
 var CACHE_NAME = 'pwa-sample-caches';
 var urlsToCache = [
-	'/pwa/',
-	'/pwa/css/style.css',
-	'/pwa/drawer.js'
+	'/',
+	'/css/style.css',
+	'/drawer.js'
 ];
 
 // インストール処理
